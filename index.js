85d27834c3ffb5b520efc1a0e2845fe2a1dc5681
Toni T. Diep <60229738+tonitdiep@users.noreply.github.com>
2021-11-03 03:02:56 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+#104 Maximum Depth of Binary Tree
+
+Instruction: Given the root of a binary tree, return its maximum depth.
+
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+
+Constraints: The number of nodes in the tree is in the range [0, 104].
+-100 <= Node.val <= 100
