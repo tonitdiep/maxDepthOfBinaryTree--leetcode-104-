39f0f0c9aafa9d15595f4dfa84d8859fa8f9fd31
Toni T. Diep <60229738+tonitdiep@users.noreply.github.com>
2021-11-03 03:04:15 +0000

    solution

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-#104 Maximum Depth of Binary Tree
+// #104 Maximum Depth of Binary Tree
 
-Instruction: Given the root of a binary tree, return its maximum depth.
+// Instruction: Given the root of a binary tree, return its maximum depth.
 
-A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+// A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
 
-Input: root = [3,9,20,null,null,15,7]
-Output: 3
+// Input: root = [3,9,20,null,null,15,7]
+// Output: 3
 
-Constraints: The number of nodes in the tree is in the range [0, 104].
--100 <= Node.val <= 100
+// Constraints: The number of nodes in the tree is in the range [0, 104].
+// -100 <= Node.val <= 100
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    if (root === null) return 0;
+    
+    
+      //^need variable declaration.
+    const leftSubtree = maxDepth(root.left);
+    const rightSubtree = maxDepth(root.right);
+
+  
+     return 1 + Math.max(leftSubtree, rightSubtree);
+   
+};
